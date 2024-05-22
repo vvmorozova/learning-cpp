15fcde2f7ad82417b743a6ea2a4b5a8bd835298b
vvmorozova <vvmorozova_1@miem.hse.ru>
2024-05-22 15:48:53 +0300

    add directory for greedy alogs

---
+++ y.pract/algos/greedy_algos/src/main.cpp
@@ -0,0 +1,53 @@
+#include "ex01.h"
+
+int findMeetingRoom(ifstream &inFile)
+{
+	vector<pair<int, int>> segments;
+    int number, x1, x2;
+
+    inFile >> number;
+    for (int i = 0; i < number; i++){
+        inFile >> x1 >> x2;
+        segments[i] = {x1, x2};
+    }
+
+    for (int i = 0; i < number; i++){
+        cout << segments[i].first << " " << segments[i].second << endl;
+    }
+    return true;
+}
+
+bool prepare(string input, string output)
+{
+	ifstream inFile, outFile;
+	string s1, s2;
+
+	inFile.open(input);
+	outFile.open(output);
+
+	outFile >> s2;
+	int answer = (s2 == "YES" ? 1 : 0);
+
+	return findMeetingRoom(inFile) == answer;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int minTest = (argc > 1 ? stoi(argv[1]) : 2), maxTest = (argc > 1 ? stoi(argv[2]) : 3);
+	string inputPath = "test_files/input/", outputPath = "test_files/output/",
+		   currDir =
+			   "/mnt/c/Users/morozovavv/Documents/learning-cpp/y.pract/3_4_adapters_n_views/ex01/";
+	vector<bool> test_rez = {};
+	for (int i = minTest; i < maxTest; i++)
+	{
+		cout << "TEST " << i << endl;
+		bool rez = prepare(currDir + inputPath + "test" + to_string(i) + ".txt",
+						   currDir + outputPath + "test" + to_string(i) + ".txt");
+		test_rez.push_back(rez);
+	}
+	for (long unsigned int i = 0; i < test_rez.size(); i++)
+	{
+		cout << "TEST " << i + minTest << (test_rez[i] ? " OK" : " FAIL") << endl;
+	}
+}
