07af7dc7e3ee1d4399b5989052fbffab4f4a36e5
v.morozova <v.morozova@rubetek.com>
2024-05-22 18:09:05 +0300

    fully correst task

--- y.pract/algos/greedy_algos/inc/greedy_algo.h
+++ y.pract/algos/greedy_algos/inc/greedy_algo.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <stack>
+#include <utility>
 
 using namespace std;
 
--- y.pract/algos/greedy_algos/src/main.cpp
+++ y.pract/algos/greedy_algos/src/main.cpp
@@ -1,34 +1,48 @@
-#include "ex01.h"
+#include "greedy_algo.h"
 
 int findMeetingRoom(ifstream &inFile)
 {
 	vector<pair<int, int>> segments;
-    int number, x1, x2;
+	int number, l, r, minL, minR, amount = 0;
 
-    inFile >> number;
-    for (int i = 0; i < number; i++){
-        inFile >> x1 >> x2;
-        segments[i] = {x1, x2};
-    }
+	inFile >> number;
+	for (int i = 0; i < number; i++)
+	{
+		inFile >> l >> r;
+		segments.push_back(std::make_pair(l, r));
+	}
+	sort(segments.begin(), segments.end(), [](pair<int, int> &a, pair<int, int> &b)
+		 { return b.second > a.second; });
 
-    for (int i = 0; i < number; i++){
-        cout << segments[i].first << " " << segments[i].second << endl;
-    }
-    return true;
+	while (segments.size() > 0)
+	{
+		minL = segments[0].first;
+		minR = segments[0].second;
+		while (((segments[0].first >= minL && segments[0].first <= minR) ||
+				(segments[0].second >= minL && segments[0].second <= minR) ||
+				(segments[0].first <= minL && segments[0].second  >= minR)) &&
+			   segments.begin() != segments.end())
+		{
+			segments.erase(segments.begin());
+		}
+		amount++;
+	}
+	cout << "my res " << amount << endl;
+	return amount;
 }
 
 bool prepare(string input, string output)
 {
 	ifstream inFile, outFile;
-	string s1, s2;
+	int res;
 
 	inFile.open(input);
 	outFile.open(output);
 
-	outFile >> s2;
-	int answer = (s2 == "YES" ? 1 : 0);
+	outFile >> res;
+	cout << "right res " << res << endl;
 
-	return findMeetingRoom(inFile) == answer;
+	return findMeetingRoom(inFile) == res;
 }
 
 int main(int argc, char *argv[])
@@ -37,7 +51,7 @@ int main(int argc, char *argv[])
 	int minTest = (argc > 1 ? stoi(argv[1]) : 2), maxTest = (argc > 1 ? stoi(argv[2]) : 3);
 	string inputPath = "test_files/input/", outputPath = "test_files/output/",
 		   currDir =
-			   "/mnt/c/Users/morozovavv/Documents/learning-cpp/y.pract/3_4_adapters_n_views/ex01/";
+			   "/home/rubetek/Documents/learning-cpp/y.pract/algos/greedy_algos/";
 	vector<bool> test_rez = {};
 	for (int i = minTest; i < maxTest; i++)
 	{
